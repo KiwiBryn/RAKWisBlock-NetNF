7c8a4548b187b1c675d51269cfa9791f52e578e5
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-08-11 14:27:49 +1200

    Add project files.

---
+++ Blinky/Program.cs
@@ -0,0 +1,94 @@
+﻿//
+// Copyright (c) .NET Foundation and Contributors
+// See LICENSE file in the project root for full license information.
+//
+//
+using System.Device.Gpio;
+using System;
+using System.Threading;
+using nanoFramework.Hardware.Esp32;
+
+namespace Blinky
+{
+    public class Program
+    {
+        private static GpioController s_GpioController;
+        public static void Main()
+        {
+            s_GpioController = new GpioController();
+
+            // pick a board, uncomment one line for GpioPin; default is STM32F769I_DISCO
+
+            // DISCOVERY4: PD15 is LED6 
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('D', 15), PinMode.Output);
+
+            // ESP32 DevKit: 4 is a valid GPIO pin in, some boards like Xiuxin ESP32 may require GPIO Pin 2 instead.
+            //GpioPin led = s_GpioController.OpenPin(4, PinMode.Output);
+
+            // FEATHER S2: 
+            //GpioPin led = s_GpioController.OpenPin(13, PinMode.Output);
+
+            // F429I_DISCO: PG14 is LEDLD4 
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('G', 14), PinMode.Output);
+
+            // NETDUINO 3 Wifi: A10 is LED onboard blue
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('A', 10), PinMode.Output);
+
+            // QUAIL: PE15 is LED1  
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('E', 15), PinMode.Output);
+
+            // STM32F091RC: PA5 is LED_GREEN
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('A', 5), PinMode.Output);
+
+            // STM32F746_NUCLEO: PB75 is LED2
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('B', 7), PinMode.Output);
+
+            //STM32F769I_DISCO: PJ5 is LD2
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('J', 5), PinMode.Output);
+
+            // ST_B_L475E_IOT01A: PB14 is LD2
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('B', 14), PinMode.Output);
+
+            // STM32L072Z_LRWAN1: PA5 is LD2
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('A', 5), PinMode.Output);
+
+            // TI CC13x2 Launchpad: DIO_07 it's the green LED
+            //GpioPin led = s_GpioController.OpenPin(7, PinMode.Output);
+
+            // TI CC13x2 Launchpad: DIO_06 it's the red LED  
+            //GpioPin led = s_GpioController.OpenPin(6, PinMode.Output);
+
+            // ULX3S FPGA board: for the red D22 LED from the ESP32-WROOM32, GPIO5
+            //GpioPin led = s_GpioController.OpenPin(5, PinMode.Output);
+
+            // Silabs SLSTK3701A: LED1 PH14 is LLED1
+            //GpioPin led = s_GpioController.OpenPin(PinNumber('H', 14), PinMode.Output);
+
+            // RAK11200
+            GpioPin led = s_GpioController.OpenPin(Gpio.IO12, PinMode.Output); // LED1 Green
+            //GpioPin led = s_GpioController.OpenPin(Gpio.IO02, PinMode.Output); // LED2 Blue
+
+            led.Write(PinValue.Low);
+
+            while (true)
+            {
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(125);
+                led.Toggle();
+                Thread.Sleep(525);
+            }
+        }
+
+        static int PinNumber(char port, byte pin)
+        {
+            if (port < 'A' || port > 'J')
+                throw new ArgumentException();
+
+            return ((port - 'A') * 16) + pin;
+        }
+    }
+}
---
+++ SHTC3/Program.cs
@@ -0,0 +1,66 @@
+//---------------------------------------------------------------------------------
+// Copyright (c) August 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// https://store.rakwireless.com/products/rak1901-shtc3-temperature-humidity-sensor
+//
+// https://github.com/nanoframework/nanoFramework.IoT.Device/tree/develop/devices/Shtc3
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RAK.Wisblock.Sht3C
+{
+    using System;
+    using System.Diagnostics;
+    using System.Device.I2c;
+    using System.Threading;
+
+    using Iot.Device.Shtc3;
+    using nanoFramework.Hardware.Esp32;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Debug.WriteLine("devMobile.IoT.RAK.Wisblock.SHTC3 starting");
+
+            try
+            {
+                Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.I2C1_DATA);
+                Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.I2C1_CLOCK);
+
+                I2cConnectionSettings settings = new(1, Shtc3.DefaultI2cAddress);
+
+                using (I2cDevice device = I2cDevice.Create(settings))
+                using (Shtc3 shtc3 = new(device))
+                {
+                    while (true)
+                    {
+                        if (shtc3.TryGetTemperatureAndHumidity(out var temperature, out var relativeHumidity))
+                        {
+                            Debug.WriteLine($"Temperature {temperature.DegreesCelsius:F1}°C  Humidity {relativeHumidity.Value:F0}%");
+                        }
+
+                        Thread.Sleep(10000);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"SHTC3 initialisation or read failed {ex.Message}");
+
+                Thread.Sleep(Timeout.Infinite);
+            }
+        }
+    }
+}
