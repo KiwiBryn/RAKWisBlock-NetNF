57759dd41154815e3bfc395483ff7d1f99be32ae
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-08-29 16:08:51 +1200

    Added (c) notice & fixed up command sent

--- EVBSerial/Program.cs
+++ EVBSerial/Program.cs
@@ -1,3 +1,18 @@
+//---------------------------------------------------------------------------------
+// Copyright (c) August 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK.LoraWAN
 { 
    using System;
@@ -58,7 +73,7 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK.LoraWAN
                   //atCommand = "AT+SN=?"; // Empty response?
                   //atCommand = "AT+HWMODEL=?";
                   //atCommand = "AT+HWID=?";
-                  atCommand = "AT+DEVEUI=?";
+                  //atCommand = "AT+DEVEUI=?";
                   //atCommand = "AT+APPEUI=?";
                   //atCommand = "AT+APPKEY=?";
                   //atCommand = "ATR";
@@ -98,6 +113,5 @@ namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK.LoraWAN
                break;
          }
       }
-
    }
 }
