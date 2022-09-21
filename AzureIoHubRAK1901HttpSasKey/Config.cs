85f5f2563460348066a64c24ca7d128dc451d2d3
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-09-21 15:30:19 +1200

    First cut of generating SAS key on the fly, will extend to refreshkey every so often

---
+++ AzureIoHubRAK1901HttpSasKey/Config.cs
@@ -0,0 +1,56 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) September 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RAK.Wisblock.AzureIoHub.RAK1901.SasKey
+{
+   using System;
+
+    public class Config
+    {
+        public const string DeviceID = "...";
+        public const string AzureIoTHubHostName = "...";
+        public const string Key = "...";
+        public readonly static TimeSpan SasKeyValidFor = new TimeSpan(24, 0, 0);
+        public readonly static TimeSpan TelemetryUploadInterval = new TimeSpan(0, 10, 0);
+
+        public const string Ssid = "...";
+        public const string Password = "...";
+
+        // DigiCert Trusted Root Authority from https://www.digicert.com/kb/digicert-root-certificates.htm#otherroots
+        public const string DigiCertBaltimoreCyberTrustRoot =
+@"-----BEGIN CERTIFICATE-----
+MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
+RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
+VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
+DTI1MDUxMjIzNTkwMFowWjELMAkGA1UEBhMCSUUxEjAQBgNVBAoTCUJhbHRpbW9y
+ZTETMBEGA1UECxMKQ3liZXJUcnVzdDEiMCAGA1UEAxMZQmFsdGltb3JlIEN5YmVy
+VHJ1c3QgUm9vdDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAKMEuyKr
+mD1X6CZymrV51Cni4eiVgLGw41uOKymaZN+hXe2wCQVt2yguzmKiYv60iNoS6zjr
+IZ3AQSsBUnuId9Mcj8e6uYi1agnnc+gRQKfRzMpijS3ljwumUNKoUMMo6vWrJYeK
+mpYcqWe4PwzV9/lSEy/CG9VwcPCPwBLKBsua4dnKM3p31vjsufFoREJIE9LAwqSu
+XmD+tqYF/LTdB1kC1FkYmGP1pWPgkAx9XbIGevOF6uvUA65ehD5f/xXtabz5OTZy
+dc93Uk3zyZAsuT3lySNTPx8kmCFcB5kpvcY67Oduhjprl3RjM71oGDHweI12v/ye
+jl0qhqdNkNwnGjkCAwEAAaNFMEMwHQYDVR0OBBYEFOWdWTCCR1jMrPoIVDaGezq1
+BE3wMBIGA1UdEwEB/wQIMAYBAf8CAQMwDgYDVR0PAQH/BAQDAgEGMA0GCSqGSIb3
+DQEBBQUAA4IBAQCFDF2O5G9RaEIFoN27TyclhAO992T9Ldcw46QQF+vaKSm2eT92
+9hkTI7gQCvlYpNRhcL0EYWoSihfVCr3FvDB81ukMJY2GQE/szKN+OMY3EU/t3Wgx
+jkzSswF07r51XgdIGn9w/xZchMB5hbgF/X++ZRGjD8ACtPhSNzkE1akxehi/oCr0
+Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
+ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
+R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
+-----END CERTIFICATE-----";
+    }
+}
---
+++ AzureIoHubRAK1901HttpSasKey/Program.cs
@@ -0,0 +1,129 @@
+//---------------------------------------------------------------------------------
+// Copyright (c) September 2022, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// https://docs.rakwireless.com/Product-Categories/WisBlock/RAK2305
+//
+// https://docs.rakwireless.com/Product-Categories/WisBlock/RAK11200
+//
+// https://store.rakwireless.com/products/rak1901-shtc3-temperature-humidity-sensor
+//
+// https://github.com/nanoframework/nanoFramework.IoT.Device/tree/develop/devices/Shtc3
+//
+// Inspired by
+// https://github.com/nanoframework/Samples/tree/main/samples/HTTP/HttpAzurePOST
+//
+// SAS Key generation was inspired by some really old posts by me which were based on the Micro Framework OBD sample
+// http://blog.devmobile.co.nz/2014/08/30/gps-tracker-azure-service-bus/
+// http://blog.devmobile.co.nz/2019/11/24/azure-iot-hub-sas-tokens-revisited-again/
+// http://blog.devmobile.co.nz/2019/11/24/azure-iot-hub-sas-keys-revisited/
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.RAK.Wisblock.AzureIoHub.RAK1901.SasKey
+{
+   using System;
+   using System.Device.I2c;
+   using System.Diagnostics;
+   using System.Net.Http;
+   using System.Security.Cryptography.X509Certificates;
+   using System.Text;
+   using System.Threading;
+   using System.Web;
+
+   using Iot.Device.Shtc3;
+
+   using ElzeKool;
+
+   using nanoFramework.Hardware.Esp32;
+   using nanoFramework.Networking;
+
+   public class Program
+   {
+      private static HttpClient _httpClient;
+
+      public static void Main()
+      {
+         Debug.WriteLine("devMobile.IoT.RAK.Wisblock.AzureIoHub.RAK1901.SasKey starting");
+
+         Configuration.SetPinFunction(Gpio.IO04, DeviceFunction.I2C1_DATA);
+         Configuration.SetPinFunction(Gpio.IO05, DeviceFunction.I2C1_CLOCK);
+
+         if (!WifiNetworkHelper.ConnectDhcp(Config.Ssid, Config.Password, requiresDateTime: true))
+         {
+            if (NetworkHelper.HelperException != null)
+            {
+               Debug.WriteLine($"WifiNetworkHelper.ConnectDhcp failed {NetworkHelper.HelperException}");
+            }
+
+            Thread.Sleep(Timeout.Infinite);
+         }
+
+         _httpClient = new HttpClient
+         {
+            SslProtocols = System.Net.Security.SslProtocols.Tls12,
+            HttpsAuthentCert = new X509Certificate(Config.DigiCertBaltimoreCyberTrustRoot),
+            BaseAddress = new Uri($"https://{Config.AzureIoTHubHostName}.azure-devices.net/devices/{Config.DeviceID}/messages/events?api-version=2020-03-13"),
+         };
+
+         string uri = $"{Config.AzureIoTHubHostName}.azure-devices.net/devices/{Config.DeviceID}";
+
+         _httpClient.DefaultRequestHeaders.Add("Authorization", GenerateSasToken(uri, Config.Key, Config.SasKeyValidFor));
+
+         I2cConnectionSettings settings = new(1, Shtc3.DefaultI2cAddress);
+         I2cDevice device = I2cDevice.Create(settings);
+         Shtc3 shtc3 = new(device);
+
+         while (true)
+         {
+            if (shtc3.TryGetTemperatureAndHumidity(out var temperature, out var relativeHumidity))
+            {
+               Debug.WriteLine($"Temperature {temperature.DegreesCelsius:F1}°C  Humidity {relativeHumidity.Value:F0}%");
+
+               string payload = $"{{\"RelativeHumidity\":{relativeHumidity.Value:F0},\"Temperature\":{temperature.DegreesCelsius.ToString("F1")}}}";
+
+               try
+               {
+                  using (HttpContent content = new StringContent(payload))
+                  using (HttpResponseMessage response = _httpClient.Post("", content))
+                  {
+                     Console.WriteLine($"{DateTime.UtcNow:HH:mm:ss} Response code:{response.StatusCode}");
+
+                     response.EnsureSuccessStatusCode();
+                  }
+               }
+               catch (Exception ex)
+               {
+                  Debug.WriteLine($"Azure IoT Hub POST failed:{ex.Message}");
+               }
+            }
+
+            Thread.Sleep(Config.TelemetryUploadInterval);
+         }
+      }
+
+      public static string GenerateSasToken(string resourceUri, string key, TimeSpan sasKeyvalidFor)
+      {
+         long sasKeyExpires = DateTime.UtcNow.Add(sasKeyvalidFor).ToUnixTimeSeconds();
+
+         string stringToSign = $"{HttpUtility.UrlEncode(resourceUri)}\n{sasKeyExpires}";
+
+         var hmac = SHA.computeHMAC_SHA256(Convert.FromBase64String(key), Encoding.UTF8.GetBytes(stringToSign));
+
+         string signature = Convert.ToBase64String(hmac);
+
+         return String.Format( "SharedAccessSignature sr={0}&sig={1}&se={2}", HttpUtility.UrlEncode(resourceUri), HttpUtility.UrlEncode(signature), sasKeyExpires);
+      }
+   }
+}
+
