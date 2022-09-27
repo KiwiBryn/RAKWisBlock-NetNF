7a435b8203a72736487a7a346d137f849ba29074
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-09-27 17:25:53 +1300

    Fixed up the namespace so matches project

--- AzureIoHubRAK1901HttpSasToken/Config.cs
+++ AzureIoHubRAK1901HttpSasToken/Config.cs
@@ -14,7 +14,7 @@
 // limitations under the License.
 //
 //---------------------------------------------------------------------------------
-namespace devMobile.IoT.RAK.Wisblock.AzureIoHub.RAK1901.Basic
+namespace devMobile.IoT.RAK.Wisblock.AzureIoHub.RAK1901.SasToken
 {
     public class Config
     {
