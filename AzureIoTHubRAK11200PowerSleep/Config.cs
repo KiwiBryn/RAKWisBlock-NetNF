118e8b47d6bf5878912ac09c2628291e1f3cca2b
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2022-10-13 14:43:40 +1300

    Added config for different delay when operation fails + noteabout certificates

--- AzureIoTHubRAK11200PowerSleep/Config.cs
+++ AzureIoTHubRAK11200PowerSleep/Config.cs
@@ -16,23 +16,27 @@
 //---------------------------------------------------------------------------------
 namespace devMobile.IoT.RAK.Wisblock.AzureIoTHub.RAK11200.PowerSleep
 {
-   using System;
+    using System;
 
-   public class Config
-   {
-      public const string DeviceID = "...";
-      public const string AzureIoTHubHostName = "...";
-      public const string Key = "...";
-      public readonly static TimeSpan SasTokenRenewFor = new TimeSpan(24, 0, 0);
-      public readonly static TimeSpan SasTokenRenewEvery = new TimeSpan(0, 30, 0);
-      public readonly static TimeSpan TelemetryUploadInterval = new TimeSpan(0, 10, 0);
+    public class Config
+    {
+        public const string DeviceID = "...";
+        public const string AzureIoTHubHostName = "...";
+        public const string Key = "...";
+        public readonly static TimeSpan SasTokenRenewFor = new TimeSpan(24, 0, 0);
+        public readonly static TimeSpan SasTokenRenewEvery = new TimeSpan(0, 30, 0);
+        public readonly static TimeSpan FailureRetryInterval = new TimeSpan(0, 5, 0);
+        public readonly static TimeSpan TelemetryUploadInterval = new TimeSpan(0, 10, 0);
 
-      public const string Ssid = "...";
-      public const string Password = "...";
+        public const string Ssid = "...";
+        public const string Password = "...";
 
-      // DigiCert Trusted Root Authority from https://www.digicert.com/kb/digicert-root-certificates.htm#otherroots
-      public const string DigiCertBaltimoreCyberTrustRoot =
-@"-----BEGIN CERTIFICATE-----
+        // DigiCert Trusted Root Authority from https://www.digicert.com/kb/digicert-root-certificates.htm#otherroots
+        //
+        // https://techcommunity.microsoft.com/t5/internet-of-things-blog/azure-iot-tls-critical-changes-are-almost-here-and-why-you/ba-p/2393169
+        //
+        public const string DigiCertBaltimoreCyberTrustRoot =
+  @"-----BEGIN CERTIFICATE-----
 MIIDdzCCAl+gAwIBAgIEAgAAuTANBgkqhkiG9w0BAQUFADBaMQswCQYDVQQGEwJJ
 RTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJlclRydXN0MSIwIAYD
 VQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTAwMDUxMjE4NDYwMFoX
@@ -53,5 +57,5 @@ Epn3o0WC4zxe9Z2etciefC7IpJ5OCBRLbf1wbWsaY71k5h+3zvDyny67G7fyUIhz
 ksLi4xaNmjICq44Y3ekQEe5+NauQrz4wlHrQMz2nZQ/1/I6eYs9HRCwBXbsdtTLS
 R9I4LtD+gdwyah617jzV/OeBHRnDJELqYzmp
 -----END CERTIFICATE-----";
-   }
+    }
 }
