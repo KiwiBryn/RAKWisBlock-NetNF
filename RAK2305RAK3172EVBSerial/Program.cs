305d3ccd317b56eaebb7f486a393e7b45d9e69f0
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2023-03-20 13:18:55 +1300

    Fixed up namespace

--- RAK2305RAK3172EVBSerial/Program.cs
+++ RAK2305RAK3172EVBSerial/Program.cs
@@ -14,108 +14,109 @@
 // limitations under the License.
 //
 //
-namespace devMobile.IoT.LoRaWAN.nanoFramework.RAK2305
-{ 
-   using System;
-   using System.Diagnostics;
-   using System.IO.Ports;
-   using System.Threading;
-   using global::nanoFramework.Hardware.Esp32;
+namespace devMobile.IoT.RAK.Wisblock.RAK2305RAK3172
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO.Ports;
+    using System.Threading;
 
-   public class Program
-   {
-      private static SerialPort _SerialPort;
+    using nanoFramework.Hardware.Esp32;
 
-      private const string SerialPortId = "COM3";
+    public class Program
+    {
+        private static SerialPort _SerialPort;
 
-      public static void Main()
-      {
-         Debug.WriteLine("devMobile.IoT.LoRaWAN.nanoFramework.RAK.LoraWAN RAK3172 EVB starting");
+        private const string SerialPortId = "COM3";
 
-         Thread.Sleep(5000);
+        public static void Main()
+        {
+            Debug.WriteLine("devMobile.IoT.RAK.Wisblock.RAK3172 EVB starting");
 
-         try
-         {
-            // set GPIO functions for COM2 (this is UART1 on RAK2305)
-            Configuration.SetPinFunction(Gpio.IO21, DeviceFunction.COM2_TX);
-            Configuration.SetPinFunction(Gpio.IO19, DeviceFunction.COM2_RX);
+            Thread.Sleep(5000);
 
-            Debug.Write("Ports:");
-            foreach (string port in SerialPort.GetPortNames())
+            try
             {
-               Debug.Write($" {port}");
-            }
-            Debug.WriteLine("");
+                // set GPIO functions for COM2 (this is UART1 on RAK2305)
+                Configuration.SetPinFunction(Gpio.IO21, DeviceFunction.COM2_TX);
+                Configuration.SetPinFunction(Gpio.IO19, DeviceFunction.COM2_RX);
 
-            using (_SerialPort = new SerialPort(SerialPortId))
+                Debug.Write("Ports:");
+                foreach (string port in SerialPort.GetPortNames())
+                {
+                    Debug.Write($" {port}");
+                }
+                Debug.WriteLine("");
+
+                using (_SerialPort = new SerialPort(SerialPortId))
+                {
+                    // set parameters
+                    _SerialPort.BaudRate = 115200;
+                    _SerialPort.Parity = Parity.None;
+                    _SerialPort.DataBits = 8;
+                    _SerialPort.StopBits = StopBits.One;
+                    _SerialPort.Handshake = Handshake.None;
+                    _SerialPort.NewLine = "\r\n";
+                    _SerialPort.ReadTimeout = 1000;
+
+                    //_SerialPort.WatchChar = '\n'; // May 2022 WatchChar event didn't fire github issue https://github.com/nanoframework/Home/issues/1035
+
+                    _SerialPort.DataReceived += SerialDevice_DataReceived;
+
+                    _SerialPort.Open();
+
+                    _SerialPort.WatchChar = '\n';
+
+                    _SerialPort.ReadExisting(); // Running at 115K2 this was necessary
+
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        string atCommand;
+                        atCommand = "AT+VER=?";
+                        //atCommand = "AT+SN=?"; // Empty response?
+                        //atCommand = "AT+HWMODEL=?";
+                        //atCommand = "AT+HWID=?";
+                        //atCommand = "AT+DEVEUI=?";
+                        //atCommand = "AT+APPEUI=?";
+                        //atCommand = "AT+APPKEY=?";
+                        //atCommand = "ATR";
+                        //atCommand = "AT+SLEEP=4000";
+                        //atCommand = "AT+ATM";
+                        //atCommand = "AT?"; // Needed an AT Command with a really long response.
+                        Debug.WriteLine("");
+                        Debug.WriteLine($"{DateTime.UtcNow:hh:mm:ss} {i} TX:{atCommand} bytes:{atCommand.Length}--------------------------------");
+                        _SerialPort.WriteLine(atCommand);
+
+                        Thread.Sleep(5000);
+                    }
+                }
+                Debug.WriteLine("Done");
+            }
+            catch (Exception ex)
             {
-               // set parameters
-               _SerialPort.BaudRate = 115200;
-               _SerialPort.Parity = Parity.None;
-               _SerialPort.DataBits = 8;
-               _SerialPort.StopBits = StopBits.One;
-               _SerialPort.Handshake = Handshake.None;
-               _SerialPort.NewLine = "\r\n";
-               _SerialPort.ReadTimeout = 1000;
-
-               //_SerialPort.WatchChar = '\n'; // May 2022 WatchChar event didn't fire github issue https://github.com/nanoframework/Home/issues/1035
-
-               _SerialPort.DataReceived += SerialDevice_DataReceived;
-
-               _SerialPort.Open();
-
-               _SerialPort.WatchChar = '\n';
-
-               _SerialPort.ReadExisting(); // Running at 115K2 this was necessary
-           
-
-               for (int i = 0; i < 5; i++)
-               {
-                  string atCommand;
-                  atCommand = "AT+VER=?";
-                  //atCommand = "AT+SN=?"; // Empty response?
-                  //atCommand = "AT+HWMODEL=?";
-                  //atCommand = "AT+HWID=?";
-                  //atCommand = "AT+DEVEUI=?";
-                  //atCommand = "AT+APPEUI=?";
-                  //atCommand = "AT+APPKEY=?";
-                  //atCommand = "ATR";
-                  //atCommand = "AT+SLEEP=4000";
-                  //atCommand = "AT+ATM";
-                  //atCommand = "AT?"; // Needed an AT Command with a really long response.
-                  Debug.WriteLine("");
-                  Debug.WriteLine($"{DateTime.UtcNow:hh:mm:ss} {i} TX:{atCommand} bytes:{atCommand.Length}--------------------------------");
-                  _SerialPort.WriteLine(atCommand);
-
-                  Thread.Sleep(5000);
-               }
+                Debug.WriteLine(ex.Message);
             }
-            Debug.WriteLine("Done");
-         }
-         catch (Exception ex)
-         {
-            Debug.WriteLine(ex.Message);
-         }
-      }
+        }
 
-      private static void SerialDevice_DataReceived(object sender, SerialDataReceivedEventArgs e)
-      {
-         SerialPort serialPort = (SerialPort)sender;
+        private static void SerialDevice_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = (SerialPort)sender;
 
-         switch (e.EventType)
-         {
-            case SerialData.Chars:
-               break;
+            switch (e.EventType)
+            {
+                case SerialData.Chars:
+                    break;
 
-            case SerialData.WatchChar:
-               string response = serialPort.ReadExisting();
-               //Debug.Write($"{DateTime.UtcNow:hh:mm:ss} RX:{response} bytes:{response.Length}");
-               Debug.Write(response);
-               break;
-            default:
-               Debug.Assert(false, $"e.EventType {e.EventType} unknown");
-               break;
-         }
-      }
-   }
+                case SerialData.WatchChar:
+                    string response = serialPort.ReadExisting();
+                    //Debug.Write($"{DateTime.UtcNow:hh:mm:ss} RX:{response} bytes:{response.Length}");
+                    Debug.Write(response);
+                    break;
+                default:
+                    Debug.Assert(false, $"e.EventType {e.EventType} unknown");
+                    break;
+            }
+        }
+    }
 }
